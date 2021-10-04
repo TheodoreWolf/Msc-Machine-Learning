cf5e599499994a1ce28ac9789bd69fb2c67a379f
Theo_Wolf <theo.wolf@successdata.ai>
2021-10-04 18:14:09 +0100

    fibonnacci

---
+++ Modules/COMP0085/test.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    a = 0
+    b = 1
+    series = []
+    for x in range(0, n):
+        series.append(a)
+        c = a + b
+        a = b
+        b = c
+
+    print(series)
+fibonacci(50)
