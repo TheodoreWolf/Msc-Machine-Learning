ecd5756fd4905fff2be2592b3a575ab880cc709f
Theo_Wolf <theo.wolf@successdata.ai>
2021-10-15 10:02:13 +0100

    cuda

--- Modules/COMP0171/Numeric example week 2.py
+++ Modules/COMP0171/Numeric example week 2.py
@@ -1,7 +1,20 @@
-"""
-Slide numeric exercise from mackay p25
-"""
-pa = 0.01
-pb = 0.95
-pab = pa*pb/(pa*pb + (1-pa)*(1-pb))
-print(pab)
+import torch.distributions as dist
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+device = torch.device('cuda')
+def bayes_example():
+    """
+    Slide numeric exercise from mackay p25
+    """
+    pa = 0.01
+    pb = 0.95
+    pab = pa*pb/(pa*pb + (1-pa)*(1-pb))
+    return pab
+
+def dist_sample():
+    X = dist.Beta(5, 4).sample().to(device)
+    print(X.cpu().numpy())
+
+dist_sample()
