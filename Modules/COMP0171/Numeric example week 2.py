d6f06ec0ed6533d4180b832feafcaf8496a84562
Theo_Wolf <theo.wolf@successdata.ai>
2021-10-16 11:00:17 +0100

    changes

--- Modules/COMP0171/Numeric example week 2.py
+++ Modules/COMP0171/Numeric example week 2.py
@@ -4,17 +4,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 device = torch.device('cuda')
+
+
 def bayes_example():
     """
     Slide numeric exercise from mackay p25
     """
     pa = 0.01
     pb = 0.95
-    pab = pa*pb/(pa*pb + (1-pa)*(1-pb))
+    pab = pa * pb / (pa * pb + (1 - pa) * (1 - pb))
     return pab
+
 
 def dist_sample():
     X = dist.Beta(5, 4).sample().to(device)
     print(X.cpu().numpy())
 
-dist_sample()
+
+if __name__ == "__main__":
+    dist_sample()
